0d5188a1ed1e79273769f863f16d01b9af0a62f0
Jonathan Davis <42720875+JonathanDavis22@users.noreply.github.com>
2019-01-12 11:10:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
+// Various package requirements
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
 var app = express();
 var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//server listening
+// Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-});
+  });
 
+// Set variable to store user data
+  var tables = [
+    {
+      routeName: "tables",
+      customerName: "",
+      phoneNumber: "",
+      customerEmail: "",
+      customerId: ""
+    },
+];
+
+// Set routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+// Post user data to server
+app.post("/api/tables", function(req, res) {
+var newtable = req.body;
+
+newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+
+console.log(newtable);
+
+characters.push(newtable);
+
+res.json(newtable);
+});
